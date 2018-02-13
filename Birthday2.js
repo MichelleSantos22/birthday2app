b0cdaa8bb839878560e175aa99e978ebe2247160
Olivia R Hardwidge <oh72183@srcs.loc>
2018-02-13 11:33:25 -0800

    olivias js

--- Birthday2.js
+++ Birthday2.js
@@ -1,2 +1,17 @@
 var button1 = document.getElementById('button1')
 var button2 = document.getElementById('button2')
+
+
+
+
+
+ var button3 = document.getElementById('button3')
+        //days till birthday
+
+        var today = new Date("2/13/18");
+        var birthday = new Date("2/15/18");
+        var days = Math.floor((birthday.getTime() - today.getTime()) / 86400000);
+        document.write("Days until birthday: ", days, "</br>");
+
+
+
