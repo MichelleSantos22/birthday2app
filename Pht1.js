900617daf1e914bf10175e2d99fa126de3264202
Phat T Quach <pq03916@srcs.loc>
2018-03-02 11:20:15 -0800

    fads

--- Pht1.js
+++ Pht1.js
@@ -1,11 +1,11 @@
-var button = document.getElementById('button');
+var button = document.getElementById('enterYourBirthdate');
 button.addEventListener('click', () => {
-    document.getElementById('output').textContent = age()
+    document.getElementById('submit1').textContent = checkFormat()
 });
 
 function getage() {
     var present = new Date(),
-        inputdate = document.getElementById('textbox').value,
+        inputdate = document.getElementById('inputBirthdate').value,
         birthdate = new Date(inputdate),
         age = Math.floor((present - birthdate) / (365 * 24 * 60 * 60 * 1000));
     return "Your age is " + age
@@ -21,16 +21,15 @@ function getage() {
    //     var nextBday = nextmiracle.getFullYear(today.getFullYear())
   //  }
     // return "There are" + getDayUntilBirthday
-}
+//
 function checkFormat(){
-    var format = document.getElementById('button').value;
+    var format = document.getElementById('inputBirthdate').value;
     var test1 = format.substring(2,3);
-    var test2 = format.substring(6,7);
-    var
-    if (test1 = ("/") && test2 = ("/") && (format.length = 10)){
-        return age
+    var test2 = format.substring(5,6);
+    if ((test1 == "/") && (test2 == "/") && (format.length = 10)){
+        return getage();
     } else{
-        return "invalid input!!!!PLease use this format mm/dd/yyyy"
+       return "invalid input!!!!PLease use this format mm/dd/yyyy"
     }
 
 }
