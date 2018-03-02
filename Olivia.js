f5755cdfff638525f2e80757d3114d26f50d9b54
Olivia R Hardwidge <oh72183@srcs.loc>
2018-03-02 10:59:26 -0800

    pretty

--- Olivia.js
+++ Olivia.js
@@ -2,18 +2,15 @@ var button = document.getElementById("button ")
 
 function getDayUntilBirthday(input) {
     var today = new Date();
-        nextyear = today.getYear + 1,
+    nextyear = today.getYear + 1,
         birthdate = new Date(birthday);
-       Birthday.setFullYear(today.getFullYear());
-    if(today.getTime()> Birthday.getTime());
-    Birthday.setFullYear(today.getFullYear() );
+    Birthday.setFullYear(today.getFullYear());
+    if (today.getTime() > Birthday.getTime());
+    Birthday.setFullYear(today.getFullYear());
 }
-
 
 var diff = birthdate.getTune() - today.getTime();
-var days = Math.floor(diff / (1000 * 60 * 60 * 24) +1 );
+var days = Math.floor(diff / (1000 * 60 * 60 * 24) + 1);
 
-  return (days);
+return (days);
 }
-
-
